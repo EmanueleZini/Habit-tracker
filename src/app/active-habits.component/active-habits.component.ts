62fe9aabeaf0eea2a0fa618c9f49a84646cfa315
emzin <emanuelezn@gmail.com>
2026-03-09 23:15:39 +0100

    Add active habits component

--- src/app/active-habits.component/active-habits.component.ts
+++ src/app/active-habits.component/active-habits.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
-  selector: 'app-active-habits.component',
+  selector: 'app-active-habits',
   imports: [],
   templateUrl: './active-habits.component.html',
   styleUrl: './active-habits.component.css',
 })
 export class ActiveHabitsComponent {
-
+  @Input() activeCount: number = 0;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header.component/header.component';
+import { ActiveHabitsComponent } from './active-habits.component/active-habits.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, HeaderComponent],
+  imports: [FormsModule, HeaderComponent, ActiveHabitsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
